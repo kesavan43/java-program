ad74e3d6ee7876d89b7908c0c99b4640b2026858
kesavan43 <kesavan709492@gmail.com>
2024-10-01 16:41:00 +0530

    Add files via upload

---
+++ interface.java
@@ -0,0 +1,18 @@
+interface printable{
+   void print();
+}
+interface showable{
+    void show();
+}
+class interface,
+{
+  public void print(){System.out.println("Hello");}
+}
+public void show()
+{System.out.println("Welcome");}
+public static void main(String[] args)
+{
+    A7 obj=new A7();
+    obj.print();
+    obj.show();
+}
